2ca2e7ec6e6766cb680f966fc148d780d747f5cc
CanferGit <156044295+CanferGit@users.noreply.github.com>
2025-09-25 21:02:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+function translator() {
+        document.getElementById('tl-menu').innerHTML ='MENÜ';
+        document.getElementById('tl-location').innerHTML ='STANDORT';
+        document.getElementById('tl-about').innerHTML ='ÜBER UNS';
+        document.getElementById('tl-thebestramenintown').innerHTML ='DIE BESTEN RAMEN DER STADT';
+        document.getElementById('tl-howtoorder').innerHTML ='WIE MAN BESTELLT';
+        document.getElementById('tl-noodletitle').innerHTML ='Wähle<br>deine Nudeln';
+        document.getElementById('tl-pickyourbrath').innerHTML ='Wähle<br>deine Brühe';
+        document.getElementById('tl-addyourtoppings').innerHTML ='Wähle<br>deine Beläge';
+        document.getElementById('tl-ourmenu').innerHTML ='UNSERE SPEISEKARTE';
+        document.getElementById('tl-chicken').innerHTML ='HÄNCHEN';
+        document.getElementById('tl-beef').innerHTML ='RIND';
+        document.getElementById('tl-shrimp').innerHTML ='GARNELEN';
+        document.getElementById('tl-chicken1').innerHTML ='HÄNCHEN';
+        document.getElementById('tl-beef1').innerHTML ='RIND';
+        document.getElementById('tl-shrimp1').innerHTML ='GARNELEN';
+        document.getElementById('tl-chicken2').innerHTML ='HÄNCHEN';
+        document.getElementById('tl-beef2').innerHTML ='RIND';
+        document.getElementById('tl-shrimp2').innerHTML ='GARNELEN';
+        document.getElementById('tl-locationheader').innerHTML ='HIER FINDEST DU UNS';
+        document.getElementById('tl-street').innerHTML ='Kapuzinerstrasse 31, IN München, 80337';
+    }
+
+    function translator2() {
+        document.getElementById('tl-menu').innerHTML ='MENU';
+        document.getElementById('tl-location').innerHTML ='LOCATION';
+        document.getElementById('tl-about').innerHTML ='ABOUT';
+        document.getElementById('tl-thebestramenintown').innerHTML ='THE BEST RAMEN IN TOWN';
+        document.getElementById('tl-howtoorder').innerHTML ='HOW TO ORDER';
+        document.getElementById('tl-noodletitle').innerHTML ='Pick<br>Your Noodle';
+        document.getElementById('tl-pickyourbrath').innerHTML ='Pick<br>Your Brath';
+        document.getElementById('tl-addyourtoppings').innerHTML ='Add<br>Your Toppings';
+        document.getElementById('tl-ourmenu').innerHTML ='OUR MENU';
+        document.getElementById('tl-chicken').innerHTML ='CHICKEN';
+        document.getElementById('tl-beef').innerHTML ='BEEF';
+        document.getElementById('tl-shrimp').innerHTML ='SHRIMP';
+        document.getElementById('tl-chicken1').innerHTML ='CHICKEN';
+        document.getElementById('tl-beef1').innerHTML ='BEEF';
+        document.getElementById('tl-shrimp1').innerHTML ='SHRIMP';
+        document.getElementById('tl-chicken2').innerHTML ='CHICKEN';
+        document.getElementById('tl-beef2').innerHTML ='BEEF';
+        document.getElementById('tl-shrimp2').innerHTML ='SHRIMP';
+        document.getElementById('tl-locationheader').innerHTML ='FIND US AT';
+        document.getElementById('tl-street').innerHTML ='Kapuzinerstrasse 31, München, BY 80337';
+}
